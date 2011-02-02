7ed3bc7bf3932ab4e58933553f6bee618ef398b0
Sang Chul Choi <goshng@yahoo.co.kr>
2011-02-01 21:30:28 -0500

    fastq conversion and filtering of the fastq file are done in my desktop. Mapping itself is done in the CAC cluster.

--- sh/run.sh
+++ sh/run.sh
@@ -95,19 +95,19 @@ SRP001481SRA=( SRR031126 SRR031127 SRR031128 SRR031129 SRR031130 SRR031131 SRR03
 # SRP001481SRA=( SRR031222 SRR031223 )
 
 for s in \${SRP001481SRA[@]}; do
-  ./fastq-dump -DB "@\\\$ac.\\\$si" -DQ "+\\\$ac.\\\$si" \\
-    -A \$s -D input/data/$READACCESSION/\$s/\$s.sra -O output
+  #./fastq-dump -DB "@\\\$ac.\\\$si" -DQ "+\\\$ac.\\\$si" \\
+    #-A \$s -D input/data/$READACCESSION/\$s/\$s.sra -O output
 
-  perl prinseq-lite.pl -fastq output/\$s.fastq \\
-    -out_format 1 \\
-    -out_good output/\$s \\
-    -out_bad null \\
-    -min_len 12 \\
-    -min_qual_score 15 \\
-    -ns_max_p 50 \\
-    -noniupac \\
-    -trim_tail_left 5 \\
-    -trim_tail_right 5
+  #perl prinseq-lite.pl -fastq output/\$s.fastq \\
+    #-out_format 1 \\
+    #-out_good output/\$s \\
+    #-out_bad null \\
+    #-min_len 12 \\
+    #-min_qual_score 15 \\
+    #-ns_max_p 50 \\
+    #-noniupac \\
+    #-trim_tail_left 5 \\
+    #-trim_tail_right 5
 
   ./segemehl.x -i input/data/$GENOMEACCESSION.idx \\
     -d input/data/$GENOMEACCESSION.fna \\
@@ -134,7 +134,7 @@ function copy-genome-to-cac {
 }
 
 function copy-reads-to-cac {
-  cp -r $READDATA $CACDATADIR
+  cp $READDATA/*.fasta $CACDATADIR
 }
 
 function copy-data {
@@ -198,15 +198,15 @@ function filter-reads {
 # fastx_nucleotide_distribution_graph.sh -i output/SRR031130.stats -o output/SRR031130-nuc.png -t "My Library"
 # fastq_quality_boxplot_graph.sh -i output/SRR031130.stats -o output/SRR031130.png -t "My Library"
 function convert-sra-to-fastq {
-  fastq-dump -DB "@\$ac.\$si" -DQ "+\$ac.\$si" -A $1 -D $HyploriREADDIR/$1/$1.sra -O output
+  fastq-dump -DB "@\$ac.\$si" -DQ "+\$ac.\$si" -A $1 -D $READDATA/$1/$1.sra -O $READDATA
   #fastq_to_fasta -Q 33 -i output/$READID.fastq -o output/$READID.fasta
   #awk 'NR%4==1 || NR%4==2' output/$READID.fastq | sed 's/@/>/' > output/$READID.fasta
 }
 
 function prinseq-a-read {
-  perl $PRINSEQ -fastq output/$1.fastq \
+  perl $PRINSEQ -fastq $READDAT/$1.fastq \
     -out_format 1 \
-    -out_good output/$1 \
+    -out_good $READDAT/$1 \
     -out_bad null \
     -min_len 12 \
     -min_qual_score 15 \
@@ -229,18 +229,33 @@ function remove-garbage {
   rm $HpyloriQUERYFASTQ
 }
 
-# SRP001481SRA=( SRR031126 SRR031127 SRR031128 SRR031129 SRR031130 SRR031131 SRR031142 SRR031143 SRR031144 SRR031145 SRR031146 SRR031178 SRR031179 SRR031180 SRR031181 SRR031182 SRR031183 SRR031184 SRR031185 SRR031186 SRR031187 SRR031188 SRR031189 SRR031190 SRR031191 SRR031192 SRR031193 SRR031194 SRR031195 SRR031196 SRR031197 SRR031198 SRR031199 SRR031200 SRR031201 SRR031202 SRR031203 SRR031204 SRR031205 SRR031206 SRR031207 SRR031208 SRR031209 SRR031210 SRR031211 SRR031212 SRR031213 SRR031214 SRR031215 SRR031216 SRR031217 SRR031218 SRR031219 SRR031220 SRR031221 SRR031222 SRR031223 SRR031224 SRR031225 SRR031226 SRR031276 SRR031277 SRR031278 SRR031279 SRR031280 SRR031281 SRR031282 SRR031283 SRR031284 SRR031285 SRR031286 SRR031287 SRR031288 SRR031289 SRR031290 SRR031291 SRR031292 SRR031293 SRR031294 SRR031295 SRR034162 SRR034163 SRR034165 SRR034166 SRR034167 SRR034168 SRR034169 SRR034170 SRR034171 SRR034172 SRR034173 SRR034174 SRR034175 SRR034176 SRR034177 SRR034178 SRR034179 SRR034180 SRR034181 SRR034182 SRR034183 SRR034185 SRR034186 SRR034187 SRR034193 SRR034195 SRR034197 SRR034198 SRR034199 SRR034201 SRR034204 SRR034207 SRR034208 SRR034209) 
+SRP001481SRA=( SRR031126 SRR031127 SRR031128 SRR031129 SRR031130 SRR031131 SRR031142 SRR031143 SRR031144 SRR031145 SRR031146 SRR031178 SRR031179 SRR031180 SRR031181 SRR031182 SRR031183 SRR031184 SRR031185 SRR031186 SRR031187 SRR031188 SRR031189 SRR031190 SRR031191 SRR031192 SRR031193 SRR031194 SRR031195 SRR031196 SRR031197 SRR031198 SRR031199 SRR031200 SRR031201 SRR031202 SRR031203 SRR031204 SRR031205 SRR031206 SRR031207 SRR031208 SRR031209 SRR031210 SRR031211 SRR031212 SRR031213 SRR031214 SRR031215 SRR031216 SRR031217 SRR031218 SRR031219 SRR031220 SRR031221 SRR031222 SRR031223 SRR031224 SRR031225 SRR031226 SRR031276 SRR031277 SRR031278 SRR031279 SRR031280 SRR031281 SRR031282 SRR031283 SRR031284 SRR031285 SRR031286 SRR031287 SRR031288 SRR031289 SRR031290 SRR031291 SRR031292 SRR031293 SRR031294 SRR031295 SRR034162 SRR034163 SRR034165 SRR034166 SRR034167 SRR034168 SRR034169 SRR034170 SRR034171 SRR034172 SRR034173 SRR034174 SRR034175 SRR034176 SRR034177 SRR034178 SRR034179 SRR034180 SRR034181 SRR034182 SRR034183 SRR034185 SRR034186 SRR034187 SRR034193 SRR034195 SRR034197 SRR034198 SRR034199 SRR034201 SRR034204 SRR034207 SRR034208 SRR034209) 
 
 # For test ...
-SRP001481SRA=( SRR031222 SRR031223 )
+# SRP001481SRA=( SRR031222 SRR031223 )
 
-function srp001481 {
-  for s in ${SRP001481SRA[@]}; do
-    convert-sra-to-fastq $s
-    prinseq-a-read $s
-    match-a-read $s
-    # remove-garbage $s
+function run-filter {
+
+  PS3="Choose the study to analyze with rnaseq analysis: "
+  select STUDYID in srp001481; do 
+    if [ "$STUDYID" == "" ];  then
+      echo -e "You need to enter something\n"
+      continue
+    else  
+      echo -e "Wait for map-analysis file system preparation...\n"
+      prepare-filesystem
+      for s in ${SRP001481SRA[@]}; do
+        convert-sra-to-fastq $s
+        prinseq-a-read $s
+        # match-a-read $s
+        # remove-garbage $s
+      done
+      echo -e "Do the next step of copy-data!\n"
+      break
+    fi
   done
+
+
 }
 
 # A bacterial genome is used to build an index file.
@@ -267,7 +282,7 @@ function index-genome {
 # Main part of the script.
 #####################################################################
 PS3="Select what you want to do with rnaseq-analysis: "
-CHOICES=( index-genome copy-data run-map srp001481 build-reads prinseq-reads filter-reads build-index match-reads )
+CHOICES=( index-genome run-filter copy-data run-map srp001481 build-reads prinseq-reads filter-reads build-index match-reads )
 select CHOICE in ${CHOICES[@]}; do 
  
   if [ "$CHOICE" == "" ];  then
@@ -276,20 +291,21 @@ select CHOICE in ${CHOICES[@]}; do
   elif [ "$CHOICE" == "index-genome" ];  then
     index-genome
     break
+  elif [ "$CHOICE" == "run-filter" ];  then
+    run-filter
+    break
   elif [ "$CHOICE" == "copy-data" ];  then
     copy-data
     break
   elif [ "$CHOICE" == "run-map" ];  then
     run-map
     break
+
   elif [ "$CHOICE" == "srp001481" ];  then
     srp001481
     break
   elif [ "$CHOICE" == "prinseq-reads" ];  then
     prinseq-reads
-    break
-  elif [ "$CHOICE" == "filter-reads" ];  then
-    filter-reads
     break
   elif [ "$CHOICE" == "build-reads" ];  then
     build-reads
