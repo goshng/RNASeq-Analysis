284a9b1df533c78b45dcbf2b420087640b4f2631
Sang Chul Choi <goshng@yahoo.co.kr>
2012-04-06 13:57:44 -0400

    BioC is updated to version 2.10 with R (ver.2.15). RNASeq Analysis is updated as well. Counting procedure is done by using BioC. Basic statistics of short reads map and counts are needed.

--- R/gff2txdb.R
+++ R/gff2txdb.R
@@ -1,8 +1,11 @@
+library(DESeq)
+library(ShortRead)
 library(rtracklayer)
 library(GenomicRanges)
 library(VariantAnnotation)
 library(GenomicFeatures)
 gffFile <- "/Volumes/Elements/Documents/Projects/mauve/bacteria/Streptococcus_mutans_UA159_uid57947/NC_004350.gff"
+gffFile <- "/v4scratch/sc2265/rnaseq/output/cornell/1/data/NC_004350.gff"
 sm.gff <- import.gff3(gffFile)
 sm.source <- sm.gff[sm.gff$type=="source",]
 sm.gene <- sm.gff[sm.gff$type=="gene",]
@@ -46,7 +49,7 @@ splicings <-
 chrominfo <-
   data.frame( chrom=names(sm.gene),
               length=end(sm.source),
-              is_circular=TRUE )
+              is_circular=FALSE )
 
 TxDb.Smutans.UA159.uid57947.knownGene <- makeTranscriptDb (transcripts, splicings, chrominfo=chrominfo)
 txdb <- TxDb.Smutans.UA159.uid57947.knownGene 
@@ -55,37 +58,153 @@ cds(txdb, columns="tx_name")
 cds(txdb, columns="exon_name")
 
 # Read short reads
-library(ShortRead)
 aln <- readAligned(bam.file,type="BAM")
 ShortRead::readAligned                   # seems to be good for exploring reads
 # AlignedRead object? from which package?
 GenomicRanges::summarizeOverlaps         # GappedAlignments is part of GenomicRanges
 Rsamtools::readBamGappedAlignments       # seems to be more efficient than readAligned
-GenomicRanges::readGappedAlignment # no such function exists.
-                                        
+GenomicRanges::readGappedAlignment # This is just calling readBamGappedalignments
 
-# Test of reads
-  group_id <- c("A", "B", "C", "C", "D", "D", "E", "F", "G", "H", "H")
-  features <- GRanges(
-      seqnames = Rle(c("chr1", "chr2", "chr1", "chr1", "chr2", "chr2", 
-          "chr1", "chr1", "chr2", "chr1", "chr1")),
-      strand = strand(rep("+", length(group_id))),
-      ranges = IRanges(
-          start=c(1000, 2000, 3000, 3600, 7000, 7500, 4000, 4000, 3000, 
-              5000, 5400),
-          width=c(500, 900, 500, 300, 600, 300, 500, 900, 500, 500, 500)),
-     DataFrame(group_id)
-  )
+# Basic statistics of short reads
+fq.file <- "/v4scratch/sc2265/rnaseq/output/cornell/1/data/FASTQ051.fq.gz"
+bam.file <- "/v4scratch/sc2265/rnaseq/output/cornell/1/bwa/FASTQ051.sorted.bam"
 
-  reads <- GappedAlignments(
-      names = c("a","b","c","d","e","f","g"),
-      seqnames = Rle(c(rep(c("chr1", "chr2"), 3), "chr1")),
-      pos = as.integer(c(1400, 2700, 3400, 7100, 4000, 3100, 5200)),
-      cigar = c("500M", "100M", "300M", "500M", "300M", 
-          "50M200N50M", "50M150N50M"),
-      strand = strand(rep("+", 7)))
+fq.file <- "/Users/goshng/Documents/Projects/RNASeq-Analysis/FASTQ051.fq.gz"
+bam.file <- "/Users/goshng/Documents/Projects/RNASeq-Analysis/FASTQ051.sorted.bam"
+indexBam(bam.file)
+# 1. Total number of short reads in fastq: zcat FASTQ051.fq.gz | wc -l
+# 15166944
+# scanBamWhat()
+# "qname"  "flag"   "rname"  "strand" "pos"    "qwidth" "mapq"   "cigar" "mrnm"   "mpos"   "isize"  "seq"    "qual"
+bam.what <- c("qname","flag","pos","mapq","cigar")
+bvAll <- scanBam(bam.file, param=ScanBamParam(what=bam.what))
+bvAll <- scanBam(bam.file, param=ScanBamParam(what=c("mapq")))
+# 1. Number of short reads in the bam file
+# > length(bvAll[[1]][["mapq"]])
+# [1] 14403971
+# $ samtools view FASTQ051.sorted.bam | wc -l
+# 14403971
+bv <- readBamGappedAlignments(bam.file,use.names=TRUE,param=ScanBamParam(what=c("mapq")))
+# 2. Number of short reads mapped 
+# > length(bv)
+# [1] 14081608
+# bv1 <- bvAll[[1]][["qname"]][!bvAll[[1]][["qname"]] %in% names(bv)]
+# What are the differences?
+# They are not actually aligned. For example,
+# samtools view FASTQ051.sorted.bam | grep HWI-ST1085\:57\:D0E15ACXX\:8\:1101\:7072\:1957
+# > table(bvAll[[1]][["flag"]])
+#       0       4      16 
+# 7033720  322363 7047888
+#   
+# Select mapped reads with mapq greater than 30
+bv <- bv[elementMetadata(bv)["mapq"][,1]>30]
+# 3. Number of uniquely mapped short reads
+# > length(bv)
+# [1] 5460982
+# Take too much time to read in fastq file: fq <- readFastq(fq.file)
+# Map the uniquely mapped reads on CDS regions
+feature.cds <- cds(txdb, columns="exon_name")
+# Remove strands information
+strand(feature.cds) <- '*'
+strand(bv) <- '*'
+stopifnot(length(runValue(seqnames(feature.cds)))==1)
+seqnames(bv) <- rep(runValue(seqnames(feature.cds)),length(bv))
+olap <- summarizeOverlaps(feature.cds, bv,mode="IntersectionStrict")
+# 4. Number of short reads uniquely mapped on CDS regions
+# > sum(assays(olap)$counts)
+# [1] 4251781
+deseq <- newCountDataSet(countData=assays(olap)$counts, conditions=c("51"))
 
-solap <- summarizeOverlaps(features, reads)
+# Flattened tx
+feature.tx.flat <- transcripts(txdb)
+strand(feature.tx.flat) <- '*'
+feature.tx.flat <- reduce(feature.tx.flat)
 
+feature.nc <- transcripts(txdb)
+strand(feature.nc) <- '*'
+feature.nc <- gaps(reduce(feature.nc))
+feature.nc <- feature.nc[-2:-1]
+# sum(width(feature.nc))
+stopifnot(length(runValue(seqnames(feature.nc)))==1)
+seqnames(bv) <- rep(runValue(seqnames(feature.nc)),length(bv))
+olap <- summarizeOverlaps(feature.nc,bv,mode="IntersectionStrict")
+# 5. Number of short reads uniquely mapped on non-coding regions
+# > sum(assays(olap)$counts)
+# [1] 510054
+sum(width(feature.nc)) + sum(width(feature.tx.flat)) 
 
+# Simulate (create) short reads
+genome.file <- "/v4scratch/sc2265/rnaseq/output/cornell/1/data/NC_004350.fna"
+fastq.file <- "test.fq"
+file.create(fastq.file)
+read.GR <- GRanges()
+
+read.loc <- function(x) { 
+  s <- sample(c(TRUE,FALSE),size=1) 
+  y <- x + 99
+  if (s == FALSE) {
+    y <- x - 99
+  }
+  if (y < 1) {
+    y <- 1
+  }
+  if (y > length(s.mutans)) {
+    y <- length(s.mutans)
+  }
+  z <- strand("+")
+  if (x > y) {
+    z <- x
+    x <- y
+    y <- z
+    z <- strand("-") 
+  }
+  list(x,y,z)
+}
+
+read.extract <- function (x,y,z,w) {
+  s <- subseq(s.mutans,x,y)
+  if (z == "-") {
+    s <- reverseComplement(s)
+  }
+  s.quality <- paste((rep("B",times=length(s))),collapse="")
+  oneRead <- sprintf("@HWI-ST397:%09d 1:N:0:ACAGTG\n%s\n+\n%s\n", w, s, s.quality)
+  cat(oneRead, file=fastq.file, append=TRUE)
+}
+
+s.mutans.sequence <- read.DNAStringSet(genome.file)
+chrom.list <- names(s.mutans.sequence)
+
+#i <- chrom.list[1]
+#i <- chrom.list[2]
+
+for (i in names(s.mutans.sequence)) { 
+  cat (i,"\\n")
+
+i <- "NC_004350.1"
+
+  geneIR <- ranges(feature.cds)
+  geneIR <- geneIR[width(geneIR)>1000]
+  geneIR <- geneIR - 100
+  if (length(geneIR) > 0) {
+    read.pos <- c(mapply(function(x,y) sample(x:y,size=10,replace=TRUE),start(geneIR),end(geneIR)))
+    s.mutans <- s.mutans.sequence[[i]]
+
+    read.start.end.strand <- mapply(read.loc, read.pos)
+    
+    one.GR <- GRanges( seqnames = Rle(i, length(read.pos)),
+                       ranges =
+                         IRanges(start=unlist(read.start.end.strand[1,]),
+                                 end=unlist(read.start.end.strand[2,]),
+                                 names=sprintf("%s.%09d",i,seq(length(read.pos)))),
+                        strand = unlist(read.start.end.strand[3,])
+                     )
+    read.GR <- c(read.GR,one.GR)
+
+    read.simulated <- mapply(read.extract, 
+                             unlist(read.start.end.strand[1,]),
+                             unlist(read.start.end.strand[2,]), 
+                             unlist(read.start.end.strand[3,]),
+                             seq(length(unlist(read.start.end.strand[3,]))))
+  }
+}
 
